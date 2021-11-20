22ebcc29280795532e0229724bb46f0a7fef0550
Arno Molenaers <arno.molenaers@student.uhasselt.be>
2021-11-20 21:10:30 +0100

    template + adjustments V1

---
+++ myMusic/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function formSubmit(Request $req)
+    {
+        print_r($req->input('email'));
+    }
+}
